3c134e19dd84a7a1311b44926b2dbbf63cbb51e8
huynhduykhang2001gv <huynhduykhang2001gv@gmail.com>
2023-06-09 18:17:19 +0700

    add about us page

--- lib/main/about_us.dart
+++ lib/main/about_us.dart
@@ -8,21 +8,20 @@ class AboutUsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
+    return SafeArea(
+        child: Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         centerTitle: true,
         leading: GestureDetector(
-          onTap: (){
+          onTap: () {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => NavigationBarScreen(currentIndex: 1)),
+              MaterialPageRoute(
+                  builder: (context) => NavigationBarScreen(currentIndex: 1)),
             );
           },
-          child: const Icon(
-              Icons.arrow_back_ios_rounded,
-              color: Colors.black
-          ),
+          child: const Icon(Icons.arrow_back_ios_rounded, color: Colors.black),
         ),
         title: const Text(
           'Về chúng tôi',
@@ -34,12 +33,108 @@ class AboutUsScreen extends StatelessWidget {
         ),
         elevation: 0,
       ),
-      body: const Center(
-        child: Text("Thức",style: TextStyle(
-            fontFamily: 'Nunito Sans',
-            fontSize: 14,
-            fontWeight: FontWeight.w500,
-            color: Colors.black)),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: [
+            const Text(
+              "Thiết bị ISport được dùng để đo hiệu suất vận động của các cầu thủ trên sân bóng và thông báo về smartphone cho đội trưởng biết về hiện trạng của đội viên.\nThiết bị sẽ được cập nhật cũng như thường xuyên để cải thiện hiệu xuất đo đạc cũng như độ chính xác. ",
+              style: TextStyle(
+                  fontFamily: 'Nunito Sans',
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black,
+                  height: 1.9),
+              textAlign: TextAlign.justify,
+            ),
+            const Text(
+              "Mọi thắc mắc vui lòng liên hệ chúng tôi qua email:",
+              style: TextStyle(
+                  fontFamily: 'Nunito Sans',
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black,
+                  height: 1.9),
+              textAlign: TextAlign.justify,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              constraints: const BoxConstraints(maxHeight: 100),
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        width: 8,
+                        height: 8,
+                        margin: const EdgeInsets.only(right: 10),
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.black),
+                      ),
+                      const Text(
+                        "vocongthuc0611@gmail.com",
+                        style: TextStyle(
+                          fontFamily: 'Nunito Sans',
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 8,
+                        height: 8,
+                        margin: const EdgeInsets.only(right: 10),
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.black),
+                      ),
+                      const Text(
+                        "19146275@st.hcmute.edu.vn",
+                        style: TextStyle(
+                          fontFamily: 'Nunito Sans',
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 8,
+                        height: 8,
+                        margin: const EdgeInsets.only(right: 10),
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.black),
+                      ),
+                      const Text(
+                        "19146280@st.hcmute.edu.vn",
+                        style: TextStyle(
+                          fontFamily: 'Nunito Sans',
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     ));
   }
